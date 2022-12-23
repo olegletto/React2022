7c7831a346d7b143b8aa6c988a20e0ab3b077ee2
Labunin Oleg (Oleg) **CTR** <olabunin@avaya.com>
2022-12-23 17:10:15 +0100

    feat: add requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import axios, { AxiosError } from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product';
+// import { products } from './data/products';
+import { IProduct } from './model';
 
 function App() {
+  const [products, setProducts] = useState<IProduct[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  async function fetchProducts() {
+    try {
+      setError('')
+      setLoading(true)
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+      setProducts(response.data)
+      setLoading(false)
+    } catch (e) {
+      const error = e as AxiosError
+      setLoading(false)
+      setError(error.message)
+    }
+
+  }
+
+useEffect( () => {
+  fetchProducts()
+},[])
+
   return (
-   <div className='container mx-auto max-w-2xl pt-5'>
-    <Product/>
-   </div>
+    <div className='container mx-auto max-w-2xl pt-5'>
+      {loading && <p className='text-center'>Loading...</p>}
+      {error && <p className='text-center text-red-600'>{error}</p>}
+      {products.map(item => <Product product={item} key={item.id} />)}
+    </div>
   );
 }
 
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,5 +1,33 @@
-export function Product() {
+import { useState } from "react"
+import { IProduct } from "../model"
+
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product({ product }: ProductProps) {
+
+    const [details, setDetails] = useState(false)
+
+    const btnClassName = details ? 'bg-blue-400' : 'bg-yellow-400'
+
+    const btnClasses = ['py-2 px-4 border', btnClassName]
     return (
-        <div>Product</div>
+        <div
+            className='border py-2 px-4 rounded flex flex-col items-center mb-2'
+        >
+            <img src={product.image} className='w-1/6' alt={product.title} />
+            <p>{product.title}</p>
+            <p className="font-bold">{product.price}</p>
+            <button
+                className={btnClasses.join(' ')}
+                onClick={() => setDetails(!details)}
+            >{details ? 'Hide details' : 'Show details'}</button>
+            {details && <div>
+                <p>{product.description}</p>
+                <p>Rate: <span style={{fontWeight: 800}}>{product.rating.rate}</span></p>
+            </div>
+            }
+        </div>
     )
 }
