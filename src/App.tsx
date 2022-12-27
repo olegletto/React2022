56ab31df7c018d28fedd8415b3da66bc1a91b18f
Labunin Oleg (Oleg) **CTR** <olabunin@avaya.com>
2022-12-27 16:24:30 +0100

    feat: create new product

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,21 @@
-import axios, { AxiosError } from 'axios';
-import React, { useEffect, useState } from 'react';
+import CreateProduct from './components/CreateProduct';
+import ErrorMessage from './components/ErrorMessage';
+import Loader from './components/Loader';
+import Modal from './components/Modal';
 import { Product } from './components/Product';
-// import { products } from './data/products';
-import { IProduct } from './model';
+import { useProducts } from './hooks/products';
 
 function App() {
-  const [products, setProducts] = useState<IProduct[]>([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-
-  async function fetchProducts() {
-    try {
-      setError('')
-      setLoading(true)
-      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-      setProducts(response.data)
-      setLoading(false)
-    } catch (e) {
-      const error = e as AxiosError
-      setLoading(false)
-      setError(error.message)
-    }
-
-  }
-
-useEffect( () => {
-  fetchProducts()
-},[])
+  const {products, error, loading} = useProducts()
 
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      {loading && <p className='text-center'>Loading...</p>}
-      {error && <p className='text-center text-red-600'>{error}</p>}
+      {loading && <Loader/>}
+      {error && <ErrorMessage error={error}/>}
       {products.map(item => <Product product={item} key={item.id} />)}
+      <Modal title="Create new product">
+        <CreateProduct/>
+      </Modal>
     </div>
   );
 }
---
+++ src/components/CreateProduct.tsx
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { IProduct } from '../model'
+import ErrorMessage from './ErrorMessage'
+
+const productData: IProduct = {
+    title: 'test product',
+    price: 13.5,
+    description: 'lorem ipsum set',
+    image: 'https://i.pravatar.cc',
+    category: 'electronic',
+    rating: {
+        rate: 10,
+        count: 1
+    }
+}
+
+export default function CreateProduct() {
+
+    const [value, setValue] = useState('')
+    const [error, setError] = useState('')
+
+    const submitHandler = async (event: React.FormEvent) => {
+        event.preventDefault()
+
+        if (value.trim().length === 0) {
+            setError('Please enter valid title')
+            return
+        }
+
+        productData.title = value
+
+        const response = await axios.post<IProduct>('https://fakestoreapi.com/products')
+    }
+
+
+
+    // ## function for onSubmit ##
+    // const changeHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    //     setValue(event.target.value)
+    // }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <input
+                type="text"
+                className='border py-2 px-4 mb-2 w-full'
+                placeholder='Enter product title...'
+                value={value}
+                onChange={event => setValue(event.target.value)}
+            />
+
+            {error && <ErrorMessage error={error} />}
+
+            <button type="submit" className='py-2 px-4 border bg-yellow-400 hover:text-white'>Create</button>
+        </form>
+    )
+}
